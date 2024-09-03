c9384e92eb87eb3283b01e9d1995fd52dda9342a
AminaB <aminatou.barry97@gmail.com>
2024-09-03 16:10:25 +0200

    DOM selectors

---
+++ js/domPractice.js
@@ -0,0 +1,7 @@
+// console.log(document.all)
+
+// let main= document.getElementById('main');
+// main.innerHTML="<h1>hello world</h1>";
+
+// document.querySelector('#main h1').innerHTML="<h1>hello world</h1>";
+
