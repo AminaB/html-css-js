4b323eaf614fcd0186087ba787c299a8e3efe508
AminaB <aminatou.barry97@gmail.com>
2024-09-04 10:27:40 +0200

    refactor create item

--- js/dom/apenChallenge.js
+++ js/dom/apenChallenge.js
@@ -13,3 +13,37 @@ button.appendChild(i);
 li.appendChild(text2);
 li.appendChild(button);
 document.querySelector('ul').appendChild(li);
+
+
+
+// refactor with many functions
+
+function createNewItem(item) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = createButton('remove-item btn-link text-red');
+
+  li.appendChild(button);
+
+  document.querySelector('.items').appendChild(li);
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+createNewItem('Cheese');
+createNewItem('Sauce');
