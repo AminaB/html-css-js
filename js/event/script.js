80c8780575703211a8187a8362c2df0d4b99e8f0
AminaB <aminatou.barry97@gmail.com>
2024-09-06 10:04:40 +0200

    event button

--- js/event/script.js
+++ js/event/script.js
@@ -131,3 +131,25 @@ function onSubmit2(e) {
 }
 
 form.addEventListener('submit', onSubmit2);
+
+// Event building
+const button = document.querySelector('form button');
+const div = document.querySelector('form div:nth-child(2)');
+const form = document.querySelector('form');
+
+button.addEventListener('click', (e) => {
+  alert('Button was clicked');
+  e.stopPropagation();
+});
+
+div.addEventListener('click', () => {
+  alert('Div was clicked');
+});
+
+form.addEventListener('click', () => {
+  alert('Form was clicked');
+});
+
+document.body.addEventListener('click', () => {
+  alert('Body was clicked');
+});
