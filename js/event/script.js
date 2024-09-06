3a6984bff7fab045eae0b83e4a531a4e5259a3a4
AminaB <aminatou.barry97@gmail.com>
2024-09-06 09:59:23 +0200

    form submission

--- js/event/script.js
+++ js/event/script.js
@@ -89,3 +89,45 @@ priorityInput.addEventListener('change', onInput);
 checkbox.addEventListener('input', onChecked);
 itemInput.addEventListener('focus', onFocus);
 itemInput.addEventListener('blur', onBlur);
+
+//form submission
+const form = document.getElementById('item-form');
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  // Get value with .value
+  const item = document.getElementById('item-input').value;
+  const priority = document.getElementById('priority-input').value;
+
+  if (item === '' || priority === '0') {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  console.log(item, priority);
+}
+
+// Using the FormData Object
+function onSubmit2(e) {
+  e.preventDefault(); // for to dont submit in the same page if there is no action
+
+  const formData = new FormData(form);
+
+  // Get individual items
+  const item = formData.get('item');
+  const priority = formData.get('priority');
+
+  console.log(item, priority);
+
+  // Get al entried as an Iterator
+  const entries = formData.entries();
+  console.log(entries);
+
+  // Loop through entries
+  for (let entry of entries) {
+    console.log(entry[1]);
+  }
+}
+
+form.addEventListener('submit', onSubmit2);
