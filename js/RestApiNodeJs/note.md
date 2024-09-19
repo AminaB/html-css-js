da04b39af1a3bf671a76bd024b0a9365ff972dca
AminaB <aminatou.barry97@gmail.com>
2024-09-19 14:57:50 +0200

    nodemon and route clean up

--- js/RestApiNodeJs/note.md
+++ js/RestApiNodeJs/note.md
@@ -2,3 +2,6 @@ expressjs.com : documentation node js app
     
 npm init
 npm i express
+
+nodeman : so we do not restart server 
+    npm i -D nodemon
--- js/RestApiNodeJs/server.js
+++ js/RestApiNodeJs/server.js
@@ -1,41 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const ideas = [
-    {
-        id: 1,
-        text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-        tag: 'Technology',
-        username: 'TonyStark',
-        date: '2022-01-02',
-    },
-    {
-        id: 2,
-        text: 'Milk cartons that turn a different color the older that your milk is getting',
-        tag: 'Inventions',
-        username: 'SteveRogers',
-        date: '2022-01-02',
-    },
-    {
-        id: 3,
-        text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2022-01-02',
-    },
-];
 app.get('/', (req, res) => {
     res.json({message:'Hello World'});
 });
-app.get('/api/ideas', (req, res) => {
-    res.json({success: true, ideas: ideas});
-})
-
-app.get('/api/ideas/:id', (req, res) => {
-    const idea=ideas.find(idea => idea.id ===+ req.params.id);
-    if(!idea){
-        return res.status(404).json({success:false, error:'Not Found'});
-    }
-    return res.json({success: true, data:idea });
-})
+const ideaRouter= require ('./routes/ideas');
+app.use('/api/ideas', ideaRouter );
 app.listen(port,()=>console.log(`Server started on port ${port}`));
