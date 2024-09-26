6ffe5e59ee5fe980fd89370b361a92f0ef86126a
AminaB <aminatou.barry97@gmail.com>
2024-09-26 23:15:07 +0200

    http routing

--- nodejs/http/app.js
+++ nodejs/http/app.js
@@ -6,13 +6,30 @@ const fs= require('fs');
 //     fs.createReadStream(__dirname+'/index.html').pipe(res);
 // }).listen(1337, '127.0.0.1');
 
+//json
+// http.createServer((req, res)=>{
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     const obj= {
+//         firstname: 'John',
+//         lastname: 'Doe',
+//     };
+//     res.end(JSON.stringify(obj));
+// }).listen(1337, '127.0.0.1');
 
 http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    const obj= {
-        firstname: 'John',
-        lastname: 'Doe',
-    };
-    res.end(JSON.stringify(obj));
-}).listen(1337, '127.0.0.1');
+    if(req.url === '/'){
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
+    }
+    else if(req.url === '/api'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const obj= {
+            firstname: 'John',
+            lastname: 'Doe',
+        };
+        res.end(JSON.stringify(obj));
+    }else {
+        res.writeHead(404);
+        res.end();
+    }
 
+}).listen(1337, '127.0.0.1');
