8caaded5cb6a9b2012d2dac951ccb98b41ccf35a
AminaB <aminatou.barry97@gmail.com>
2024-09-26 23:01:09 +0200

    http using stream

--- nodejs/http/app.js
+++ nodejs/http/app.js
@@ -3,8 +3,5 @@ const fs= require('fs');
 
 http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type': 'text/html'});
-    let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-    const message='Hello World...';
-    html= html.replace('{Message}',message);
-    res.end(html);
+    fs.createReadStream(__dirname+'/index.html').pipe(res);
 }).listen(1337, '127.0.0.1');
