76f6d17f10825fc4ddd93a54a41650f9edb5ee79
AminaB <aminatou.barry97@gmail.com>
2024-09-30 11:28:43 +0200

    React : conditionals in jsx

--- reactJs/feedback-app/src/App.js
+++ reactJs/feedback-app/src/App.js
@@ -7,14 +7,19 @@ function App() {
         {id:2, text :'Comment two'},
         {id:3, text :'Comment three'}
     ]
+    const loading=false;
+    const showComments=false;
+    if(loading){ return <h1>Loading...</h1>}
     return (
         <div className="App">
             <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
-            {Math.random()*5+5}
+           {/*// {Math.random()*5+5}*/}
+            {showComments ? 'yes': 'non'}
             <div>
                 <h3> comments ({comments.length})</h3>
                 <ul>
+                    i
                     {comments.map((comment, index) => (
                         <li key={index}> {comment.text}</li>
 
