27fe28fa3ca65c9004f3842caad88798be6aa6b1
AminaB <aminatou.barry97@gmail.com>
2024-09-30 11:23:14 +0200

    React : dynamic values

--- reactJs/feedback-app/src/App.js
+++ reactJs/feedback-app/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 function App() {
-    return React.createElement('div', {className:'container'},
-            React.createElement('h1', {},'My App')
-        )
+    const title= 'Blog Post';
+    const body = 'This is my blog post'
+    const comments=[
+        {id:1, text :'Comment one'},
+        {id:2, text :'Comment two'},
+        {id:3, text :'Comment three'}
+    ]
+    return (
+        <div className="App">
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            {Math.random()*5+5}
+            <div>
+                <h3> comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}> {comment.text}</li>
 
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default App;
