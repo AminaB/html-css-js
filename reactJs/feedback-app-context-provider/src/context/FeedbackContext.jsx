32ee2bc6f43d9f699a639f8f6f4bb5c45dfff133
AminaB <aminatou.barry97@gmail.com>
2024-10-02 10:13:46 +0200

    fix React : json server, fetch data from json server

--- reactJs/feedback-app-context-provider/src/context/FeedbackContext.jsx
+++ reactJs/feedback-app-context-provider/src/context/FeedbackContext.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
-import {useState, createContext} from "react";
+import {useState, createContext, useEffect} from "react";
 import {v4 as uuidv4} from "uuid";
 
 const FeedbackContext=createContext(undefined)
 
 export const FeedbackProvider=({children})=>{
-    const [feedback,setFeedback]=useState([
-        {
-            id: 1,
-            text: 'This item 1 is from context',
-            rating: 10
-        },
-        {
-            id: 2,
-            text: 'This item 2 is from context',
-            rating: 9
-        },
-        {
-            id: 3,
-            text: 'This item  is from context',
-            rating: 9
-        }
-    ]);
+    const [feedback,setFeedback]=useState([]);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item:{},
         edit: false
     })
+    useEffect(()=>{
+        fetchFeedback();
+    },[])
+
+    // fetcch data
+    const fetchFeedback= async ()=>{
+        const response= await fetch('http://localhost:5000/feedback?_sort=id&_order=desc');
+        const data =await response.json();
+        setFeedback(data);
+    }
     // edi item
     const editFeedback = (item)=>{
         setFeedbackEdit({item, edit:true});
