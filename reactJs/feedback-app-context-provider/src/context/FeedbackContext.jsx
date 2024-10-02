8a43d9f3056f949570f0b3aada3280de13da9ccc
AminaB <aminatou.barry97@gmail.com>
2024-10-02 10:43:03 +0200

    fix React : json server, delete add update item

--- reactJs/feedback-app-context-provider/src/context/FeedbackContext.jsx
+++ reactJs/feedback-app-context-provider/src/context/FeedbackContext.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {useState, createContext, useEffect} from "react";
-import {v4 as uuidv4} from "uuid";
 
 const FeedbackContext=createContext(undefined)
 
@@ -27,20 +26,37 @@ export const FeedbackProvider=({children})=>{
         setFeedbackEdit({item, edit:true});
     }
     // Update feedback item
-    const updateFeedback=(id,updItem)=>{
+    const updateFeedback= async (id,updItem)=>{
+       const response= await fetch(`/feedback/${id}`,{
+            method:'PUT',
+            headers:{
+                contentType:'application/json',
+            },
+            body:JSON.stringify(updItem)
+        })
+        const data= await response.json();
         setFeedback(feedback.map((item)=>
-             (item.id===+id? {...item, ...updItem}: item))
+             (item.id===id? {...item, ...data}: item))
         )
     }
-    const deleteFeedBack = (id) => {
+    const deleteFeedBack = async (id) => {
         if(window.confirm("Are you sure you want to delete this feedback?")){
+            await fetch(`/feedback/${id}`,
+                {method:'DELETE',})
             setFeedback(feedback.filter((item) => item.id !== id))
         }
 
     }
-    const addFeedback = (newFeedBack) => {
-        newFeedBack.id=uuidv4();
-        setFeedback([newFeedBack,...feedback]);
+    const addFeedback = async (newFeedBack) => {
+        const response= await fetch('/feedback/', {
+            method: 'POST',
+            headers: {
+                contentType: 'application/json',
+            },
+            body: JSON.stringify(newFeedBack)
+        });
+        const data=await response.json();
+        setFeedback([data,...feedback]);
     }
     return <FeedbackContext.Provider  value={{
         feedback,
